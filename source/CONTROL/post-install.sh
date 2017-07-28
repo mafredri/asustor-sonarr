edc12f704f6777bc61e8362a60b83b3361e37c8d
Mathias Fredriksson <mathias@walkbase.com>
2017-07-29 01:32:27 +0300

    Fix (un)install scripts for ADM 3.0

--- source/CONTROL/post-install.sh
+++ source/CONTROL/post-install.sh
@@ -1,46 +1,46 @@
 #!/bin/sh
 
 if [ -z "$APKG_PKG_DIR" ]; then
-    PKG_DIR=/usr/local/AppCentral/sonarr
+	PKG_DIR=/usr/local/AppCentral/sonarr
 else
-    PKG_DIR=$APKG_PKG_DIR
+	PKG_DIR=$APKG_PKG_DIR
 fi
 
 # Source env variables
 . "${PKG_DIR}/CONTROL/env.sh"
 
 install_sonarr() {
-    tar xzf "$PKG_DIR"/.release/NzbDrone.*.tar.gz -C "$PKG_DIR/"
-    mv "$PKG_DIR/NzbDrone" "$PKG_DIR/Sonarr"
+	tar xzf "$PKG_DIR"/.release/NzbDrone.*.tar.gz -C "$PKG_DIR/"
+	mv "$PKG_DIR/NzbDrone" "$PKG_DIR/Sonarr"
 }
 
 case "${APKG_PKG_STATUS}" in
-    install)
-        if [ ! -f "$PKG_CONF/config.xml" ]; then
-            cp "$PKG_CONF/config_base.xml" "$PKG_CONF/config.xml"
-        fi
+	install)
+		if [ ! -f "$PKG_CONF/config.xml" ]; then
+			cp "$PKG_CONF/config_base.xml" "$PKG_CONF/config.xml"
+		fi
 
-        # If NzbDrone configuration exists, copy it
-        if [ -d /volume1/home/admin/.config/NzbDrone ]; then
-            rsync -ra --exclude "config.xml" /volume1/home/admin/.config/NzbDrone/ "$PKG_CONF/"
-        fi
+		# If NzbDrone configuration exists, copy it
+		if [ -d /volume1/home/admin/.config/NzbDrone ]; then
+			rsync -ra --exclude "config.xml" /volume1/home/admin/.config/NzbDrone/ "$PKG_CONF/"
+		fi
 
-        chown -R "$DAEMON_USER" "$PKG_CONF"
+		chown -R "$DAEMON_USER" "$PKG_CONF"
 
-        install_sonarr
-        ;;
-    upgrade)
-        rsync -ra "$APKG_TEMP_DIR/config/" "$PKG_CONF/"
+		install_sonarr
+		;;
+	upgrade)
+		rsync -ra "$APKG_TEMP_DIR/config/" "$PKG_CONF/"
 
-        # Restore application or extract included version
-        if [ -d "$APKG_TEMP_DIR/Sonarr" ]; then
-            cp -af "$APKG_TEMP_DIR/Sonarr" "$PKG_DIR/"
-        else
-            install_sonarr
-        fi
-        ;;
-    *)
-        ;;
+		# Restore application or extract included version
+		if [ -d "$APKG_TEMP_DIR/Sonarr" ]; then
+			cp -af "$APKG_TEMP_DIR/Sonarr" "$PKG_DIR/"
+		else
+			install_sonarr
+		fi
+		;;
+	*)
+		;;
 esac
 
 chown -R "$DAEMON_USER" "$PKG_DIR/Sonarr"
--- source/CONTROL/pre-install.sh
+++ source/CONTROL/pre-install.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
 if [ -z "$APKG_PKG_DIR" ]; then
-    PKG_DIR=/usr/local/AppCentral/sonarr
+	PKG_DIR=/usr/local/AppCentral/sonarr
 else
-    PKG_DIR=$APKG_PKG_DIR
+	PKG_DIR=$APKG_PKG_DIR
 fi
 
-# Source env variables
-. "${PKG_DIR}/CONTROL/env.sh"
-
 case "${APKG_PKG_STATUS}" in
-    install)
-        ;;
-    upgrade)
-        # Back up configuration
-        rsync -ra --exclude "config_base.xml" "$PKG_CONF/" "$APKG_TEMP_DIR/config/"
+	install)
+		;;
+	upgrade)
+		# Source env variables
+		. "${PKG_DIR}/CONTROL/env.sh"
 
-        # Back up application
-        if [ -d "$PKG_DIR/Sonarr" ]; then
-            cp -af "$PKG_DIR/Sonarr" "$APKG_TEMP_DIR/"
-        fi
-        ;;
-    *)
-        ;;
+		# Back up configuration
+		rsync -ra --exclude "config_base.xml" "$PKG_CONF/" "$APKG_TEMP_DIR/config/"
+
+		# Back up application
+		if [ -d "$PKG_DIR/Sonarr" ]; then
+			cp -af "$PKG_DIR/Sonarr" "$APKG_TEMP_DIR/"
+		fi
+		;;
+	*)
+		;;
 esac
 
 exit 0
